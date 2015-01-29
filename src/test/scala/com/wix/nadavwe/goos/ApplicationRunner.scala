4effbd52291d9f5b0cd05c9b6ea7eca7ef14818c
Nadav Wexler <nadavwe@wix.com>
2015-01-29 16:17:50 +0200

    separated auction server from application runner

--- src/test/scala/com/wix/nadavwe/goos/ApplicationRunner.scala
+++ src/test/scala/com/wix/nadavwe/goos/ApplicationRunner.scala
@@ -60,56 +60,6 @@ class AuctionSniperDriver(timeoutMillis:Int) extends JFrameDriver(
   }
 }
 
-class FakeAuctionServer(val itemId:String) extends MustThrownMatchers {
-  import Main._
-  private val AUCTION_PASSWORD = "auction"
-
-  private val connection = new XMPPConnection(Constants.XMPPHostname)
-  private var currentChat:Chat = _
-  private val messageListener = new SingleMessageListener()
-
-  def startSellingItem() {
-    connection.connect()
-    connection.login(ITEM_ID_AS_LOGIN.format(itemId), AUCTION_PASSWORD, AUCTION_RESOURCE)
-    connection.getChatManager().addChatListener(
-    new ChatManagerListener() {
-      override def chatCreated(chat:Chat, createdLocally:Boolean) {
-        currentChat = chat
-        chat.addMessageListener(messageListener)
-      }
-    })
-  }
-
-  def reportPrice(price: Int, increment: Int, bidder: String) = {
-    currentChat.sendMessage(s"SOLVersion: 1.1; Event: PRICE; " +
-      s"CurrentPrice: $price; Increment: $increment; Bidder: $bidder;")
-  }
-
-  def hasReceivedJoinRequestFromSniper() { messageListener.receivesAMessage(anything)  }
-
-
-  def hasReceivedBid(bid: Int, bidderId: String) = {
-    currentChat.getParticipant must beEqualTo(bidderId)
-
-    currentChat must beWithParticipant(bidderId)
-    messageListener.receivesAMessage(beEqualTo(s"SOLVersion: 1.1; Command: BID; Price: $bid;"))
-  }
-
-
-  def announceClosed()  { currentChat.sendMessage(new Message())  }
-  def stop() { connection.disconnect()  }
-
-}
-
-class SingleMessageListener extends MessageListener with MustThrownMatchers {
-  private val messages = new ArrayBlockingQueue[Message](1)
-  def processMessage(chat:Chat, message:Message) { messages.add(message) }
-  def receivesAMessage(bodyMatcher: Matcher[String]) {
-    val message = messages.poll(5, TimeUnit.SECONDS)
-    message must not(beNull)
-    message must haveBodyThat(bodyMatcher)
-    }
-}
 
 
 
