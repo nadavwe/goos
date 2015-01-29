4b626894c5d82efd7e945c9cdbdd3e18d7205837
Nadav Wexler <nadavwe@wix.com>
2015-01-29 15:07:54 +0200

    first IT test works.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,18 +8,17 @@ import com.wix.nadavwe.goos.e2es.Constants
 import org.jivesoftware.smack.packet.Message
 import org.jivesoftware.smack.{MessageListener, Chat, XMPPConnection}
 
-class Main(hostname:String, username:String, password:String, itemId:String) {
+class Main(hostname:String, username:String, password:String) {
   import Main._
 
+  private var notToBeGCed : Chat = _
   private var ui : MainWindow = _
   startUserInterface()
+
   private val connection = connectTo()
-  startXmppConnection()
 
   private def startUserInterface() {
-    SwingUtilities.invokeAndWait(new Runnable {
-      override def run() =  {ui = new MainWindow}
-    })
+    SwingUtilities.invokeAndWait({ui = new MainWindow})
   }
 
   private def connectTo() : XMPPConnection = {
@@ -29,17 +28,20 @@ class Main(hostname:String, username:String, password:String, itemId:String) {
     connection
   }
 
-  private def startXmppConnection()  {
+  private def joinAuction(itemId:String)  {
     val chat = connection.getChatManager.createChat(
-      auctionId,
+      auctionId(itemId),
       new MessageListener {
-        override def processMessage(aChat:Chat, message: Message) {}
+        override def processMessage(aChat:Chat, message: Message) = SwingUtilities.invokeLater(ui.showStatus(Main.StatusLost))
       }
     )
+
+    notToBeGCed = chat
+
     chat.sendMessage(new Message)
   }
 
-  def auctionId = AUCTION_ID_FORMAT.format(itemId, Constants.XMPPHostname)
+  def auctionId(itemId:String) = AUCTION_ID_FORMAT.format(itemId, Constants.XMPPHostname)
 
 }
 
@@ -61,6 +63,10 @@ class MainWindow extends JFrame("AuctionSniper") {
     result
   }
 
+  def showStatus(status:String): Unit = {
+    sniperStatus.setText(status)
+  }
+
 
 }
 
@@ -73,16 +79,23 @@ object Main {
 
 
   def main(args:Array[String]): Unit = main(args:_*)
-  def main(args: String*): Unit = { new Main(args(ARG_HOSTNAME), args(ARG_USERNAME), args(ARG_PASSWORD), args(ARG_ITEM_ID)) }
+  def main(args: String*): Unit = {
+    val mainObj = new Main(args(ARG_HOSTNAME), args(ARG_USERNAME), args(ARG_PASSWORD))
+    mainObj.joinAuction(args(ARG_ITEM_ID))
+  }
 
   val MAIN_WINDOW_NAME: String = "Auction Sniper Main"
   val SNIPER_STATUS_NAME: String = "sniper status"
 
   val StatusJoining = "Joining"
-  val StatusLost = ""
+  val StatusLost = "Lost"
 
   val ITEM_ID_AS_LOGIN = "auction-%s"
   val AUCTION_RESOURCE = "Auction"
   val AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
+
+  implicit def createRunnable(f: => Unit): Runnable = new Runnable {
+    override def run(): Unit = f
+  }
 
 }
